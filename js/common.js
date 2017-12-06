54572e63bd1c8eef2ce9ebdcee1b0d52f1c7b2e0
Denis Polyarush <92azov92@gmail.com>
2017-12-06 15:28:13 +0200

    first commit

---
+++ js/common.js
@@ -0,0 +1,33 @@
+$('.one').on('click', function(){
+    var fai = $('.features_aside_item').removeClass('current');
+    var fmi = $('.features_main_item').removeClass('show');
+    $(this).addClass('current');
+    $('.main_one').addClass('show');
+})
+$('.two').on('click', function(){
+    var fai = $('.features_aside_item').removeClass('current');
+    var fmi = $('.features_main_item').removeClass('show');
+    $(this).addClass('current');
+    $('.main_two').addClass('show');
+})
+$('.tree').on('click', function(){
+    var fai = $('.features_aside_item').removeClass('current');
+    var fmi = $('.features_main_item').removeClass('show');
+    $(this).addClass('current');
+    $('.main_tree').addClass('show');
+})
+$('.four').on('click', function(){
+    var fai = $('.features_aside_item').removeClass('current');
+    var fmi = $('.features_main_item').removeClass('show');
+    $(this).addClass('current');
+    $('.main_four').addClass('show');
+})
+$('.five').on('click', function(){
+    var fai = $('.features_aside_item').removeClass('current');
+    var fmi = $('.features_main_item').removeClass('show');
+    $(this).addClass('current');
+    $('.main_five').addClass('show');
+})
+//$('.carousel').carousel({
+//  interval: 5000
+//})
